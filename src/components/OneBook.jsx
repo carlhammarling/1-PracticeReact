1439daad960c5314159adb5c36b115fe4841eeed
Carl Hammarling <carlhammarling@gmail.com>
2023-04-24 16:02:09 +0200

    Transitions

--- src/components/OneBook.jsx
+++ src/components/OneBook.jsx
@@ -11,14 +11,10 @@ const OneBook = ( { book, showBooks }) => {
     <div className={`oneBook ${showBooks ? "on" : "off"}`} onClick={() => setShowDescription(state => !state)} >
         <h3> {book.book} </h3>
         <h4> {book.author} </h4>
-        {/* {
-            showDescription
-            ? <p> {book.description} </p>
-            : <p className="clickDescription">Click to show description.</p>
-        } */}
-          <p className={`clickDescription ${showDescription ? "off" : "on"}`}>Click to show description.</p>
-          <p className={`bookDescription ${showDescription ? "on" : "off"}`}>{book.description}</p>
         
+        <p className={`clickDescription ${showDescription ? "off" : "on"}`}>Click to show description.</p>
+        <p className={`bookDescription ${showDescription ? "on" : "off"}`}>{book.description}</p>
+
         <img src= {book.imgURL} alt="" />
     </div>
   )
